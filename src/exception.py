f87d84c3f7972789dce13b33293a7f42042b335c
Vikramjois <vikram.jois7@gmail.com>
2025-11-15 14:29:54 +0530

    EDA and Model training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     #exc_tb will provide info like on which file and line the exception has occured
@@ -17,4 +18,12 @@ class customException(Exception):
 
     def __str__(self):
         return self.error_message    
+    
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise customException(e,sys)      
 
