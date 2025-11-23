abd66ec2abcae1034a972cd3f4a4015292dc67f5
Vikramjois <vikram.jois7@gmail.com>
2025-11-23 17:48:54 +0530

    Data Transformtion done

--- src/logger.py
+++ src/logger.py
@@ -14,8 +14,33 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-# if __name__=="__main__":
-#     logging.info("Logging has started") #Example to check if logging is happening or not
+if __name__=="__main__":
+    logging.info("Logging has started") #Example to check if logging is happening or not
 
      
+# # src/logger.py
+# import logging
+# import os
+# from datetime import datetime
+
+# # Directory to hold logs
+# LOG_DIR = os.path.join(os.getcwd(), "logs")
+# os.makedirs(LOG_DIR, exist_ok=True)
+
+# # Timestamped logfile name
+# LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+# LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# # Create logger
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     handlers=[
+#         logging.FileHandler(LOG_FILE_PATH, encoding="utf-8"),
+#         logging.StreamHandler(),  # prints to console
+#     ],
+# )
+
+# # Optionally export a module-level logger instance if you prefer:
+# logger = logging.getLogger(__name__)
 
