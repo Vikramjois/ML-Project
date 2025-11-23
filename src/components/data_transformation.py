7ebbf33941290280001bb37b7e364b60a64cfa3c
Vikramjois <vikram.jois7@gmail.com>
2025-11-23 18:39:39 +0530

    Model Trainer class

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,7 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
-
+    
 from src.exception import customException
 from src.logger import logging
 import os
